3ebf5930fd87a85b520bdd2944c69341aeaae8cd
ota_yuki <ota_yuki@interprism.co.jp>
2020-03-31 15:51:42 +0900

    承認者をListで持つように修正

--- src/main/java/jp/co/interprism/leaveapp/domain/承認/承認.java
+++ src/main/java/jp/co/interprism/leaveapp/domain/承認/承認.java
@@ -5,13 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.Date;
+import java.util.List;
 
 @AllArgsConstructor
 @Getter
 public class 承認 {
     private final int 申請ID;
     private final int 承認ID;
-    private final 承認者[] 承認者リスト;
+    private final List<承認者> 承認者リスト;
     private final Date 承認日;
     private final ステータス 申請承認ステータス;
 }
